4b9a2bea9321b5d4de1a45403d8910a794865ac2
SangHuPark <tkdgn5408407@gmail.com>
2023-10-18 14:48:09 +0900

    revert commit

--- src/main/java/project/GuestHouse/domain/dto/user/UserJoinRequest.java
+++ src/main/java/project/GuestHouse/domain/dto/user/UserJoinRequest.java
@@ -1,13 +1,11 @@
 package project.GuestHouse.domain.dto.user;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import project.GuestHouse.domain.entity.ProviderType;
 import project.GuestHouse.domain.entity.User;
 import project.GuestHouse.domain.entity.UserType;
 
 import javax.validation.constraints.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -27,15 +25,14 @@ public class UserJoinRequest {
     @Size(min = 2, message = "이름은 2자 이상 입력하세요.")
     private String userName;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-    private LocalDate birth;
+    @NotBlank(message = "생일을 입력하세요.")
+    private LocalDateTime birth;
 
     @NotBlank(message = "올바른 형식의 닉네임이 아닙니다.")
     @Size(min = 2, max = 10, message = "닉네임의 길이는 2자 이상 10자 이하로 입력하세요.")
     private String nickname;
 
-    @NotBlank(message = "전화번호를 입력하세요.")
-    private String phoneNum;
+    private int phoneNum;
 
     public User toEntity(String password) {
         return User.builder()
