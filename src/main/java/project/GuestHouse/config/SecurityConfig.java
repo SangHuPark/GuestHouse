a2c4f73f913e71770adc59943b4087793e95e03e
SangHuPark <tkdgn5408407@gmail.com>
2023-10-18 14:21:45 +0900

    passwordEncoder 분리

--- src/main/java/project/GuestHouse/config/SecurityConfig.java
+++ src/main/java/project/GuestHouse/config/SecurityConfig.java
@@ -1,26 +1,29 @@
 package project.GuestHouse.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import project.GuestHouse.jwt.JwtFilter;
+import project.GuestHouse.service.UserService;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserService userService;
 
     @Value("${jwt.secret}")
     private String secretKey;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -29,16 +32,16 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers("/users/join", "/users/login").permitAll() // join, login 허용
+                .antMatchers("/api/users/join", "/api/users/login").permitAll() // join, login 허용
                 .antMatchers(HttpMethod.POST,"/**").authenticated()
                 .antMatchers(HttpMethod.DELETE,"/**").authenticated()
                 .antMatchers(HttpMethod.PUT,"/**").authenticated()
-                .antMatchers(HttpMethod.GET,"/alarms").authenticated()
+//                .antMatchers(HttpMethod.GET,"/alarms").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
                 .and()
-//                .addFilterBefore(new JwtFilter(secretKey), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용
+                .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용
                 .build();
     }
 
