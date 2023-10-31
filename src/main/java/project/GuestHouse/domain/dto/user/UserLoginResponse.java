688faa89a0d2f493324d26a935e941da59433b58
SangHuPark <tkdgn5408407@gmail.com>
2023-10-31 23:33:43 +0900

    email 추가

--- src/main/java/project/GuestHouse/domain/dto/user/UserLoginResponse.java
+++ src/main/java/project/GuestHouse/domain/dto/user/UserLoginResponse.java
@@ -2,10 +2,11 @@ package project.GuestHouse.domain.dto.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 public class UserLoginResponse {
-
+    private String email;
     private String jwt;
 }
