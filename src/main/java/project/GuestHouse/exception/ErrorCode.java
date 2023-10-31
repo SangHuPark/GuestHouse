f20e1af827d5dd4c58c760e7845be8dfb82d3322
SangHuPark <tkdgn5408407@gmail.com>
2023-10-31 23:31:51 +0900

    USER_ID_NOT_FOUND 추가

--- src/main/java/project/GuestHouse/exception/ErrorCode.java
+++ src/main/java/project/GuestHouse/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "올바른 비밀번호가 아닙니다."),
     NICKNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 닉네임이 존재하지 없습니다."),
-    USER_EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자 email 입니다.");
+    USER_EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자 email 입니다."),
+    USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자 입니다.");
 
 
     private final HttpStatus httpStatus;
