e063ecf745a1718716feea27913865516991f527
SangHuPark <tkdgn5408407@gmail.com>
2023-11-04 22:13:33 +0900

    initial commit

--- src/main/java/project/GuestHouse/jwt/JwtTokenProvider.java
+++ src/main/java/project/GuestHouse/jwt/JwtTokenProvider.java
@@ -44,6 +44,7 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+
     public Authentication getAuthentication(String token) {
         UserDetails userDetails = userDetailService.loadUserByUsername(this.getUserPk(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
