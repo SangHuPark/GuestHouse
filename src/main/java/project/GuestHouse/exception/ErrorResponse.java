8a48c0dcfe2e9bcb0186987dd12d2f84a2e271e4
SangHuPark <tkdgn5408407@gmail.com>
2023-11-05 21:46:17 +0900

    initial commit

---
+++ src/main/java/project/GuestHouse/exception/ErrorResponse.java
@@ -0,0 +1,62 @@
+package project.GuestHouse.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Builder
+@Getter
+public class ErrorResponse {
+
+    private final boolean success = false;
+    private final HttpStatus httpStatus;
+    private final int code;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL) // errors 가 없다면 응답으로 내려가지 않도록 JsonInclude 어노테이션 추가. (NON_NULL 시 null 인 데이터를 제외)
+    private final List<ValidationError> errors;
+
+    public static ErrorResponse of (HttpStatus httpStatus,int code, String message) {
+        return ErrorResponse.builder()
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    public static ErrorResponse of (HttpStatus httpStatus,int code, String message, BindingResult bindingResult) {
+        return ErrorResponse.builder()
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .errors(ValidationError.of(bindingResult))
+                .build();
+    }
+
+    @Getter
+    public static class ValidationError{
+        private final String field;
+        private final String value;
+        private final String message;
+
+        private ValidationError(FieldError fieldError){
+            this.field = fieldError.getField();
+            this.value = fieldError.getRejectedValue() == null? "" :fieldError.getRejectedValue().toString() ;
+            this.message = fieldError.getDefaultMessage();
+        }
+
+        public static List<ValidationError> of(final BindingResult bindingResult){
+            return bindingResult.getFieldErrors().stream()
+                    .map(ValidationError :: new)
+                    .collect(Collectors.toList());
+        }
+    }
+}
