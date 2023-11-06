94f2251b0decf3f1e384f2fe35abccbc32cb1291
SangHuPark <tkdgn5408407@gmail.com>
2023-11-06 20:31:55 +0900

    initial commit

--- src/main/java/project/GuestHouse/service/UserService.java
+++ src/main/java/project/GuestHouse/service/UserService.java
@@ -6,52 +6,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import project.GuestHouse.domain.dto.user.UserDto;
-import project.GuestHouse.domain.dto.user.UserJoinRequest;
-import project.GuestHouse.domain.dto.user.UserLoginRequest;
-import project.GuestHouse.domain.dto.user.UserLoginResponse;
+import project.GuestHouse.domain.dto.user.*;
 import project.GuestHouse.domain.entity.User;
 import project.GuestHouse.exception.ErrorCode;
 import project.GuestHouse.exception.GuestException;
 import project.GuestHouse.jwt.JwtTokenProvider;
 import project.GuestHouse.repository.UserRepository;
-import project.GuestHouse.util.ValidateUtil;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    private final ValidateUtil validateUtil;
     private final BCryptPasswordEncoder encoder;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder encoder, JwtTokenProvider jwtTokenProvider) {
-        this.userRepository = userRepository;
-        this.encoder = encoder;
-        this.validateUtil = ValidateUtil.builder()
-                .userRepository(userRepository)
-                .build();
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
-    public UserDto createUser(UserJoinRequest userJoinRequest) {
-        userRepository.findUserByEmail(userJoinRequest.getEmail())
+    public Long createUser(UserJoinRequest userJoinRequest) {
+        userRepository.findByEmail(userJoinRequest.getEmail())
                 .ifPresent(user -> {
                     throw new GuestException(ErrorCode.DUPLICATED_USER_EMAIL);
                 });
 
         User savedUser = userRepository.save(userJoinRequest.toEntity(encoder.encode(userJoinRequest.getPassword())));
 
-        return savedUser.toDto();
+        return savedUser.getId();
     }
 
     public UserLoginResponse login(UserLoginRequest userLoginRequest) {
         String email = userLoginRequest.getEmail();
 
-        User user = validateUtil.validateUser(email);
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new GuestException(ErrorCode.USER_EMAIL_NOT_FOUND));
 
         if (!encoder.matches(userLoginRequest.getPassword(), user.getPassword())) {
             throw new GuestException(ErrorCode.INVALID_PASSWORD);
@@ -62,17 +51,22 @@ public class UserService {
         return new UserLoginResponse(email, token);
     }
 
-    public List<User> findAllUser() {
-        return userRepository.findAll();
+    public List<UserSearchResponse> findAllUser() {
+        List<User> users = userRepository.findAll();
+        List<UserSearchResponse> userList = users.stream()
+                .map(UserSearchResponse::new)
+                .collect(Collectors.toList());
+        return userList;
     }
 
-    public User findUserByEmail(String email) {
-        return userRepository.findUserByEmail(email)
+    public UserSearchResponse findUserByEmail(String email) {
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new GuestException(ErrorCode.USER_EMAIL_NOT_FOUND));
+        return new UserSearchResponse(user);
     }
 
     public boolean updateUserPassword(Long id, String password) {
-        Optional<User> result = userRepository.findUserById(id);
+        Optional<User> result = userRepository.findById(id);
         // null 이 아니면 get()의 결과가 넘어온다.
         User user = result.orElseThrow(() -> new GuestException(ErrorCode.USER_ID_NOT_FOUND));
         user.updatePassword(password);
@@ -81,7 +75,7 @@ public class UserService {
     }
 
     public boolean deleteUser(Long id) {
-        Optional<User> result = userRepository.findUserById(id);
+        Optional<User> result = userRepository.findById(id);
         User user = result.orElseThrow(() -> new GuestException(ErrorCode.USER_ID_NOT_FOUND));
         userRepository.delete(user);
         return true;
