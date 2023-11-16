0501b84227c3e7499d7512cac57ffc311f7ee023
SangHuPark <tkdgn5408407@gmail.com>
2023-11-16 13:25:31 +0900

    @Email message 추가, password/nickname pattern 적용

--- src/main/java/project/GuestHouse/domain/dto/user/UserJoinRequest.java
+++ src/main/java/project/GuestHouse/domain/dto/user/UserJoinRequest.java
@@ -8,16 +8,16 @@ import project.GuestHouse.domain.entity.UserType;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Getter @Setter
 public class UserJoinRequest {
 
-    @NotBlank(message = "올바른 형식의 이메일이 아닙니다.")
-    @Email
+    @NotBlank
+    @Email(message = "올바른 형식의 이메일이 아닙니다.")
     private String email;
 
     @NotBlank(message = "올바른 형식의 비밀번호가 아닙니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16 길이의 영문 대/소문자, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 
     @NotBlank(message = "올바른 형식의 이름이 아닙니다.")
@@ -29,6 +29,7 @@ public class UserJoinRequest {
 
     @NotBlank(message = "올바른 형식의 닉네임이 아닙니다.")
     @Size(min = 2, max = 10, message = "닉네임의 길이는 2자 이상 10자 이하로 입력하세요.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10 자리여야 합니다.")
     private String nickname;
 
     private String phoneNum;
