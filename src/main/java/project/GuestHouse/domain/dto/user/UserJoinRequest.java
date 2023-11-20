1765f2e7f4e378e0c0c6ed1894410acf25a21955
SangHuPark <tkdgn5408407@gmail.com>
2023-11-20 21:31:33 +0900

    nickname 추가, birth type 수정

--- src/main/java/project/GuestHouse/domain/dto/user/UserJoinRequest.java
+++ src/main/java/project/GuestHouse/domain/dto/user/UserJoinRequest.java
@@ -18,29 +18,33 @@ public class UserJoinRequest {
     private String email;
 
     @NotBlank(message = "올바른 형식의 비밀번호가 아닙니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16 길이의 영문 대/소문자, 숫자, 특수문자를 포함해야 합니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16 길이의 영문, 숫자, 특수문자를 포함해야 합니다.")
+//    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16 길이의 영문, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 
     @NotBlank(message = "올바른 형식의 이름이 아닙니다.")
     @Size(min = 2, message = "이름은 2자 이상 입력하세요.")
     private String userName;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-    private LocalDate birth;
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private String birth;
 
-    /*@NotBlank(message = "올바른 형식의 닉네임이 아닙니다.")
+    @NotBlank(message = "올바른 형식의 닉네임이 아닙니다.")
     @Size(min = 2, max = 10, message = "닉네임의 길이는 2자 이상 10자 이하로 입력하세요.")
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10 자리여야 합니다.")
-    private String nickname;*/
+    private String nickname;
+
+    private MultipartFile profileImg;
 
     private String phoneNum;
 
-    public User toEntity(String password) {
+    public User toEntity(String password, String profileImg, LocalDate birth) {
         return User.builder()
                 .email(this.email)
                 .password(password)
                 .userName(this.userName)
-                .birth(this.birth)
+                .birth(birth)
+                .profileImg(profileImg)
                 .phoneNum(this.phoneNum)
                 .provider(ProviderType.LOCAL) // default = LOCAL
                 .userType(UserType.NORMAL) // default = NORMAL
