cfa2d2707b92adcd097f21a9b91a5ae32212b6b9
SangHuPark <tkdgn5408407@gmail.com>
2023-11-24 22:48:59 +0900

    주석 수정

--- src/main/java/project/GuestHouse/repository/UserRepository.java
+++ src/main/java/project/GuestHouse/repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 // JpaRepository 에 @Repository 로 빈에 등록하고 있어 별도의 어노테이션이 필요 없음
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 사용자 이메일로 조회
+    // 사용자 이메일 로 조회
     Optional<User> findByEmail(String email);
 
     // 사용자 pk 로 조회
