882517a4fb61971e84278aba0b975244006b593a
SangHuPark <tkdgn5408407@gmail.com>
2023-11-27 22:38:47 +0900

    INTERNAL_SERVER_ERROR 추가

--- src/main/java/project/GuestHouse/exception/ErrorCode.java
+++ src/main/java/project/GuestHouse/exception/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     NICKNAME_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "사용자 닉네임이 존재하지 없습니다."),
     USER_EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "존재하지 않는 사용자 email 입니다."),
     USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "존재하지 않는 사용자 id 입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Internal Server Error"),
     // JWT
     TOKEN_EXPIRED_ERROR(HttpStatus.UNAUTHORIZED,401,"토큰이 만료되었습니다"),
     TOKEN_SIGNATURE_ERROR(HttpStatus.UNAUTHORIZED,401,"유효하지 않은 토큰입니다."),
