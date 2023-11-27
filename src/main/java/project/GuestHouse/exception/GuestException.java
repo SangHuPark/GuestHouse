9c967674417ea7af2defb8dd97d4f3e1b9eec11d
SangHuPark <tkdgn5408407@gmail.com>
2023-11-27 22:41:10 +0900

    AllArgsConstructor -> RequiredArgsConstructor 수정

--- src/main/java/project/GuestHouse/exception/GuestException.java
+++ src/main/java/project/GuestHouse/exception/GuestException.java
@@ -1,11 +1,11 @@
 package project.GuestHouse.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class GuestException extends RuntimeException {
 
     private ErrorCode errorCode;
