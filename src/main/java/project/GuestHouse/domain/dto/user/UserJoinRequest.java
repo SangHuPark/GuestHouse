9123b4820e6f7dd68534197df0d18d772d432afb
SangHuPark <tkdgn5408407@gmail.com>
2023-11-29 20:25:58 +0900

    delete userName, userNickname, birth

--- src/main/java/project/GuestHouse/domain/dto/user/UserJoinRequest.java
+++ src/main/java/project/GuestHouse/domain/dto/user/UserJoinRequest.java
@@ -1,6 +1,5 @@
 package project.GuestHouse.domain.dto.user;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 import project.GuestHouse.domain.entity.ProviderType;
@@ -19,9 +18,9 @@ public class UserJoinRequest {
 
     @NotBlank(message = "올바른 형식의 비밀번호가 아닙니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16 길이의 영문, 숫자, 특수문자를 포함해야 합니다.")
-//    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16 길이의 영문, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 
+    /*
     @NotBlank(message = "올바른 형식의 이름이 아닙니다.")
     @Size(min = 2, message = "이름은 2자 이상 입력하세요.")
     private String userName;
@@ -32,12 +31,29 @@ public class UserJoinRequest {
     private String userNickname;
 
 //    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    @Nullable
     private String birth;
+    */
 
     private MultipartFile profileImg;
 
     private String phoneNum;
 
+    public User toEntity(String password, LocalDate birth) {
+        return User.builder()
+                .email(this.email)
+                .password(password)
+//                .userName(userName)
+                .userName("defaultUserName")
+//                .userNickname(userNickname)
+                .userNickname("defaultUserNickname")
+                .birth(birth)
+                .phoneNum(this.phoneNum)
+                .provider(ProviderType.LOCAL) // default = LOCAL
+                .userType(UserType.NORMAL) // default = NORMAL
+                .build();
+    }
+    /*
     public User toEntity(String password, LocalDate birth) {
         return User.builder()
                 .email(this.email)
@@ -50,4 +66,5 @@ public class UserJoinRequest {
                 .userType(UserType.NORMAL) // default = NORMAL
                 .build();
     }
+    */
 }
