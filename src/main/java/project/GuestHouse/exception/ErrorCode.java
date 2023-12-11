c418c7a5f1ee56aaec3791965004c38c45d818e9
SangHuPark <tkdgn5408407@gmail.com>
2023-12-11 23:03:49 +0900

    add DUPLICATED_USER_PHONENUM

--- src/main/java/project/GuestHouse/exception/ErrorCode.java
+++ src/main/java/project/GuestHouse/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
 
     // API
     DUPLICATED_USER_EMAIL(HttpStatus.CONFLICT, 409, "이미 가입한 사용자 email 입니다."),
+    DUPLICATED_USER_PHONENUM(HttpStatus.CONFLICT, 409, "이미 가입한 사용자 phoneNum 입니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, 401, "올바른 비밀번호가 아닙니다."),
     NICKNAME_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "사용자 닉네임이 존재하지 없습니다."),
     USER_EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "존재하지 않는 사용자 email 입니다."),
