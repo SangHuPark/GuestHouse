bea310895b73b6860549f719d9e5a3a8f44ee828
SangHuPark <tkdgn5408407@gmail.com>
2023-12-13 21:17:20 +0900

    add columns name

--- src/main/java/project/GuestHouse/domain/entity/Image.java
+++ src/main/java/project/GuestHouse/domain/entity/Image.java
@@ -17,10 +17,13 @@ public class Image {
     @Column(name = "image_id")
     private Long id;
 
+    @Column(name = "origin_name")
     private String originName; // 이미지 파일의 본래 이름
 
+    @Column(name = "stored_name")
     private String storedName; // S3에 저장된 이미지 파일 이름
 
+    @Column(name = "access_url")
     private String accessUrl; // S3 URL
 
     @OneToOne
