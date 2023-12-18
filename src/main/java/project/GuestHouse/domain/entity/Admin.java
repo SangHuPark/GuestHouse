b18e1b224e3f49f027a39551bdefa1742c4812a7
SangHuPark <tkdgn5408407@gmail.com>
2023-12-18 18:23:47 +0900

    create Admin Entity

--- src/main/java/project/GuestHouse/domain/entity/Admin.java
+++ src/main/java/project/GuestHouse/domain/entity/Admin.java
@@ -3,6 +3,7 @@ package project.GuestHouse.domain.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -11,13 +12,27 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Table(name = "admin")
-public class Admin {
+public class Admin extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "admin_id")
     private Long id;
 
+    @Column(name = "admin_email", nullable = false, unique = true, length = 45)
+    private String adminEmail;
 
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "admin_name", nullable = false, length = 20)
+    private String adminName;
+
+    @Column(name = "admin_nickname", nullable = false, length = 20)
+    private String adminNickname;
+
+    @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL)
+    @JoinColumn(name = "accommodation_id")
+    private List<Accommodation> accommodation;
 
 }
