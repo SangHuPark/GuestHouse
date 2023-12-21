3c7161c37bf2b779a0533b7b8a47522548633f0d
SangHuPark <tkdgn5408407@gmail.com>
2023-12-21 16:17:37 +0900

    edit USER_ID_NOT_FOUND message

--- src/main/java/project/GuestHouse/exception/ErrorCode.java
+++ src/main/java/project/GuestHouse/exception/ErrorCode.java
@@ -14,7 +14,7 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, 401, "올바른 비밀번호가 아닙니다."),
     NICKNAME_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "사용자 닉네임이 존재하지 없습니다."),
     USER_EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "존재하지 않는 사용자 email 입니다."),
-    USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "존재하지 않는 사용자 id 입니다."),
+    USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "존재하지 않는 사용자 입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Internal Server Error"),
     // JWT
     TOKEN_EXPIRED_ERROR(HttpStatus.UNAUTHORIZED,401,"토큰이 만료되었습니다"),
