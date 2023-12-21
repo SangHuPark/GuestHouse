709715c7654c218de812e3bdaac716fc9ff37a15
SangHuPark <tkdgn5408407@gmail.com>
2023-12-21 16:19:42 +0900

    add findByUserId

--- src/main/java/project/GuestHouse/repository/UserImageRepository.java
+++ src/main/java/project/GuestHouse/repository/UserImageRepository.java
@@ -1,8 +1,12 @@
 package project.GuestHouse.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.GuestHouse.domain.entity.User;
 import project.GuestHouse.domain.entity.UserImage;
+
+import java.util.Optional;
 
 public interface UserImageRepository extends JpaRepository<UserImage, Long> {
 
+    Optional<UserImage> findByUserId(Long userId);
 }
